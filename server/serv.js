455eab7893a4af253cb3f4cee56855a83e7b5324
BluePichu <bluepichu64@gmail.com>
2014-04-13 22:22:28 -0400

    Server fixes, highlighting for changed answers.

--- server/serv.js
+++ server/serv.js
@@ -8,95 +8,95 @@ var utils = require("utils")
 var args = process.argv
 
 if (args[2] && ( args[2] == "-h" || args[2] == "--help") ) {
-	console.log("Usage servjs [index file] [port]")
-	return
+    console.log("Usage servjs [index file] [port]")
+    return
 }
 
 var pages = {
-login:"views/login.html",
-register:"views/register.html",
-
+    login:"views/login.html",
+    register:"views/register.html",
+    
 }
 
 http.createServer(function(req,res) {
-	if (req.method == "GET") {
-		var url = req.url.substr(1) || args[2] || "views/login.html"
-		if (pages[url]) {
-			url = pages[url]
-		}
-		var cT = getMime(url.split(".")[1])
-		console.log("Request received for: "+url);
-		res.writeHead(200, {'Content-Type': cT});
-		getFile(url, function(data) {
-			res.write(data);
-			res.end();
-		})
-	} else if (req.method == "POST") {
-		fullBody = "";
-		req.on('data', function(chunk) {
-			// append the current chunk of data to the fullBody variable
-			fullBody += chunk.toString();
-		});
-		
-		req.on('end', function() {
-		
-			res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-
-			var decodedBody = querystring.parse(fullBody);
-
-			if (true) { //check credentials
-				getFile("views/splash.html", function(data) {
-					res.write(data);
-					res.end();
-				})	
-			}
-		});
-	}
+    if (req.method == "GET") {
+        var url = req.url.substr(1) || args[2] || "views/login.html"
+        if (pages[url]) {
+            url = pages[url]
+        }
+        var cT = getMime(url.split(".")[1])
+        console.log("Request received for: "+url);
+        res.writeHead(200, {'Content-Type': cT});
+        getFile(url, function(data) {
+            res.write(data);
+            res.end();
+        })
+    } else if (req.method == "POST") {
+        fullBody = "";
+        req.on('data', function(chunk) {
+            // append the current chunk of data to the fullBody variable
+            fullBody += chunk.toString();
+        });
+        
+        req.on('end', function() {
+            
+            res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+            
+            var decodedBody = querystring.parse(fullBody);
+            
+            if (true) { //check credentials
+                getFile("views/splash.html", function(data) {
+                    res.write(data);
+                    res.end();
+                })	
+            }
+        });
+    }
 }).listen(args[3] || 1337, '0.0.0.0');
 
 console.log('Server running');
 
 var getFile = function(url,cb) {
-	url = url.split("..")
-	url = "public/"+url[url.length-1];
-	fs.readFile(url, function(err, data) {
-		if (err || !data) {
-			cb("404")
-			return
-		}
-		cb(data);
-	})
+    url = url.split("..")
+    url = "../public/"+url[url.length-1];
+    fs.readFile(url, function(err, data) {
+        if (err || !data) {
+            cb("404")
+            return
+        }
+        cb(data);
+    })
 }
 
 var getMime = function(str) {
-	if (!str) {
-		return "text/plain";
-	}
-	str = str.toLowerCase();
-	switch (str) {
-		case "html":
-			return "text/html"
-		case "txt":
-			return "text/plain"
-		case "js":
-		case "min":
-		case "json":
-			return "application/javascript"
-		case "gif":
-			return "image/gif"
-		case "jpeg":
-		case "jpg":
-			return "image/jpeg"
-		case "png":
-		case "bmp":
-		case "ico":
-			return "image/png"
-		case "css":
-			return "text/css"
-		case "xml":
-			return "text/xml"
-		default:
-			return "text/html"
-
-	}
+    if (!str) {
+        return "text/plain";
+    }
+    str = str.toLowerCase();
+    switch (str) {
+        case "html":
+            return "text/html"
+        case "txt":
+            return "text/plain"
+        case "js":
+        case "min":
+        case "json":
+            return "application/javascript"
+        case "gif":
+            return "image/gif"
+        case "jpeg":
+        case "jpg":
+            return "image/jpeg"
+            case "png":
+        case "bmp":
+        case "ico":
+            return "image/png"
+        case "css":
+            return "text/css"
+        case "xml":
+            return "text/xml"
+        default:
+            return "text/html"
+            
+    }
 }
